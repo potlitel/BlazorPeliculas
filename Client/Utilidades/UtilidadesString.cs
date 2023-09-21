92f4dd3c2f9604132d90069233812831d917c55a
potlitel <potlitel@gmail.com>
2023-09-21 17:04:39 -0400

    Practicando características básicas de Blazor.

---
+++ Client/Utilidades/UtilidadesString.cs
@@ -0,0 +1,7 @@
+namespace BlazorPeliculas.Client.Utilidades
+{
+    public class UtilidadesString
+    {
+        public static string Transformar(string valor) => valor.ToUpper();
+    }
+}
---
+++ Shared/Entidades/Pelicula.cs
@@ -0,0 +1,8 @@
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; } = null!;
+        public DateTime FechaLanzamiento { get; set; }
+    }
+}
