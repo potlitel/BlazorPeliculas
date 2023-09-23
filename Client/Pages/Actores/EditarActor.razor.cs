234973d9a64d9e2de5f87b8617bd2ee7eb2cebde
potlitel <potlitel@gmail.com>
2023-09-23 13:45:51 -0400

    Creando componentes de entidades del sistema.

---
+++ Client/Pages/Actores/EditarActor.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorPeliculas.Client.Pages.Actores
+{
+    public partial class EditarActor
+    {
+        public EditarActor()
+        {
+
+        }
+    }
+}
