33565c5f9356e5e45667d055c926362f21e72959
potlitel <potlitel@gmail.com>
2023-09-23 11:25:47 -0400

    Refactorizando la sección de películas.

--- Client/Repositorios/Repositorio.cs
+++ Client/Repositorios/Repositorio.cs
@@ -11,14 +11,24 @@ namespace BlazorPeliculas.Client.Repositorios
                 new Pelicula
                 {
                     Titulo = "<b>Wakanda Forever</b>",
-                    FechaLanzamiento = new DateTime(2023, 9, 11)
+                    FechaLanzamiento = new DateTime(2023, 9, 11),
+                    Poster =
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Black_Panther_Wakanda_Forever_poster.jpg/220px-Black_Panther_Wakanda_Forever_poster.jpg"
                 },
                 new Pelicula
                 {
                     Titulo = "<i>Moana</i>",
-                    FechaLanzamiento = new DateTime(2016, 11, 13)
+                    FechaLanzamiento = new DateTime(2016, 11, 13),
+                    Poster =
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Moana_Teaser_Poster.jpg/220px-Moana_Teaser_Poster.jpg"
                 },
-                new Pelicula { Titulo = "Inception", FechaLanzamiento = new DateTime(2010, 7, 16) }
+                new Pelicula
+                {
+                    Titulo = "Inception",
+                    FechaLanzamiento = new DateTime(2010, 7, 16),
+                    Poster =
+                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg"
+                }
             };
         }
     }
--- Shared/Entidades/Pelicula.cs
+++ Shared/Entidades/Pelicula.cs
@@ -4,5 +4,27 @@ namespace BlazorPeliculas.Shared.Entidades
     {
         public string Titulo { get; set; } = null!;
         public DateTime FechaLanzamiento { get; set; }
+
+        public string Poster { get; set; } = null!;
+
+        public string TituloCortado
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Titulo))
+                {
+                    return null;
+                }
+
+                if (Titulo.Length > 60)
+                {
+                    return Titulo.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Titulo;
+                }
+            }
+        }
     }
 }
