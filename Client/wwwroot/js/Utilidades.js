44a3e71a1956cdf790d5ac5ab1d7b3488533636c
potlitel <potlitel@gmail.com>
2023-09-30 15:44:23 -0400

    Se completa funcionalidad de deslogueo Automático.

--- Client/wwwroot/js/Utilidades.js
+++ Client/wwwroot/js/Utilidades.js
@@ -8,3 +8,18 @@
 function pruebaPuntoNetInstancia(dotnetHelper) {
     dotnetHelper.invokeMethodAsync("IncrementCount");
 }
+
+function timerInactivo(dotnetHelper) {
+    var timer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(logout, 3 * 1000);
+    }
+
+    function logout() {
+        dotnetHelper.invokeMethodAsync("Logout");
+    }
+}
